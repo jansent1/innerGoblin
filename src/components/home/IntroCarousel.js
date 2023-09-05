02d503f6170eae1a02d1389f83c3c3641997d4d4
Teun Jansen <teunjansen91@gmail.com>
2023-09-05 22:25:54 +0200

    Eerste opzet nieuwe intro carousel

--- src/components/home/IntroCarousel.js
+++ src/components/home/IntroCarousel.js
@@ -9,14 +9,12 @@ const IntroCarousel = () => {
           <Row>
             <Col sm={2}>
               <Card>
-                <Card.Img
-                  variant="top"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4O47QeWbJ73MU5d2cWap98MV4ewX3iUx6_g&usqp=CAU"
-                />
+                <Card.Img variant="top" src="img/fox-cop.jpg" />
                 <Card.Body>
-                  <Card.Title>Project 1</Card.Title>
+                  <Card.Title>Fox Cop</Card.Title>
                   <Card.Text>
-                    Een voorbeeldproject met een korte beschrijving.
+                    Fox-Cop, de verleidelijke goblin, betreedt de duisternis met
+                    een sluwe glimlach en verleidelijke blik in haar ogen.
                   </Card.Text>
                   <Button variant="primary" href="#">
                     Meer informatie
@@ -26,14 +24,13 @@ const IntroCarousel = () => {
             </Col>
             <Col sm={2}>
               <Card>
-                <Card.Img
-                  variant="top"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv2x6RwO5xh-Xq853CkEznG4lRZnGN9MxYMQ&usqp=CAU"
-                />
+                <Card.Img variant="top" src="img/Seadragon-gal.jpg" />
                 <Card.Body>
-                  <Card.Title>Project 2</Card.Title>
+                  <Card.Title>Sea Dragon</Card.Title>
                   <Card.Text>
-                    Een ander voorbeeldproject met een korte beschrijving.
+                    SeaDragon Girl betovert onder de diepe zee met haar
+                    gracieuze verschijning, een oceaanprinses wier staart
+                    schittert als kostbare schelpen.
                   </Card.Text>
                   <Button variant="primary" href="#">
                     Meer informatie
@@ -44,14 +41,13 @@ const IntroCarousel = () => {
             {/* Voeg hier meer projectkaarten toe */}
             <Col sm={2}>
               <Card className="active">
-                <Card.Img
-                  variant="top"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIm_oFiNANCahnp7pBVFKsn_6FjaV2hxBFcg&usqp=CAU"
-                />
+                <Card.Img variant="top" src="img/gob-monk.jpg" />
                 <Card.Body>
-                  <Card.Title>Project 1</Card.Title>
+                  <Card.Title>Ds Gob</Card.Title>
                   <Card.Text>
-                    Een voorbeeldproject met een korte beschrijving.
+                    In de serene stilte van de kloosterzalen mediteert Gob Monk
+                    met een rustige geest, een meester in de goblinkunst van
+                    innerlijke balans en vrede.
                   </Card.Text>
                   <Button variant="primary" href="#">
                     Meer informatie
@@ -61,14 +57,13 @@ const IntroCarousel = () => {
             </Col>
             <Col sm={2}>
               <Card>
-                <Card.Img
-                  variant="top"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIVJCtOXKgknwPwcaVPgtDJUQ-irQutS7S0w&usqp=CAU"
-                />
+                <Card.Img variant="top" src="img/burner-gob.jpg" />
                 <Card.Body>
-                  <Card.Title>Project 2</Card.Title>
+                  <Card.Title>Burned</Card.Title>
                   <Card.Text>
-                    Een ander voorbeeldproject met een korte beschrijving.
+                    Met een meedogenloze blik omarmt Burner-Gob het
+                    vernietigende vuur van haar enorme kanon, gereed om chaos te
+                    veroorzaken in het heetst van de strijd.
                   </Card.Text>
                   <Button variant="primary" href="#">
                     Meer informatie
@@ -78,14 +73,13 @@ const IntroCarousel = () => {
             </Col>
             <Col sm={2}>
               <Card>
-                <Card.Img
-                  variant="top"
-                  src="https://i.pinimg.com/originals/4f/80/57/4f8057f0e75dff31cfabd12ab0740d4a.jpg"
-                />
+                <Card.Img variant="top" src="img/orc-milf.jpg" />
                 <Card.Body>
-                  <Card.Title>Project 1</Card.Title>
+                  <Card.Title>Orc Mama</Card.Title>
                   <Card.Text>
-                    Een voorbeeldproject met een korte beschrijving.
+                    In het goblinrijk straalt een sexy geklede goblinmoeder als
+                    een verleidelijke ster, haar gewaagde outfit trekt
+                    bewonderende blikken.
                   </Card.Text>
                   <Button variant="primary" href="#">
                     Meer informatie
@@ -95,14 +89,13 @@ const IntroCarousel = () => {
             </Col>
             <Col sm={2}>
               <Card>
-                <Card.Img
-                  variant="top"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlUrWrVZe7SrIs5kD4sHyXIc_8e9THYneJEA&usqp=CAU"
-                />
+                <Card.Img variant="top" src="img/Eh.jpg" />
                 <Card.Body>
-                  <Card.Title>Project 2</Card.Title>
+                  <Card.Title>Ehh..</Card.Title>
                   <Card.Text>
-                    Een ander voorbeeldproject met een korte beschrijving.
+                    In het goblinrijk valt een tiener op met haar grote paarse
+                    kuif en verwarde blik. Haar onhandige charme en
+                    eigenzinnigheid maken haar een intrigerende verschijning.
                   </Card.Text>
                   <Button variant="primary" href="#">
                     Meer informatie
